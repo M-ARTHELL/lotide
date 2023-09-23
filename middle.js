6aff32acb81adc1da761b8ce2287441236360529
M-ARTHELL <makoarthell@proton.me>
2023-09-23 14:36:38 -0300

    Added explanatory comments

--- middle.js
+++ middle.js
@@ -33,14 +33,18 @@ const eqArrays = function(arr1, arr2) {
 //middle
 const middle = function(array) {
   let arrayMiddle = [];
+  //Returns empty array if it's too short.
   if (array.length === 1 || array.length === 2) {
     return console.log(arrayMiddle);
+    //Push two middle elements for even arrays.
   } else if (array.length % 2 === 0) {
     arrayMiddle.push(array[(array.length / 2) - 1]);
     arrayMiddle.push(array[array.length / 2]);
   } else {
+    //Push middle elements for other arrays
     arrayMiddle.push(array[Math.floor(array.length / 2)]);
   }
+  //Print middle arrays to console.
   return console.log(arrayMiddle);
 }
 
