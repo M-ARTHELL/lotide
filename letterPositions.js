3ad7dfad9b1f64a295350ae3dfd5cbb8d7ce51f3
M-ARTHELL <makoarthell@proton.me>
2023-09-27 08:30:05 -0300

    letterPositions.js implemented

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,20 @@
 const letterPositions = function(string) {
+  //create blank object template
   const results = {};
+  //look through letters of string
+  for (let i = 0; i < string.length; i++) {
+    //if there's a space, do nothing
+    if (string[i] === ' ') {
+      //nothing
+    //if letter doesn't exist, add it
+    } else if (results.hasOwnProperty(string[i]) == false) {
+      results[string[i]] = [i];
+    //if letter exists, push index to the correct array in object
+    } else {
+      results[string[i]].push(i);
+    }
+  }
+  return results;
 };
 
 console.log(letterPositions("lighthouse in the house"));
