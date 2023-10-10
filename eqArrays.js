0f03c9f6672de430772a44d5a97262e6078044da
M-ARTHELL <makoarthell@proton.me>
2023-10-10 02:52:55 -0300

    eqArrays: refactored + moved tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,9 @@
-//assertEqual copy
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
-  } else {
-    return console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
-  }
-};
+//assertEquals
+const assertEqual = require('./assertEqual');
 
 //eqArrays
 const eqArrays = function(arr1, arr2) {
+  let outcome = false;
   //Checks if array lengths are equal, returns false if not.
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -16,15 +11,13 @@ const eqArrays = function(arr1, arr2) {
       if (arr1[i] !== arr2[i]) {
         return false;
       } else {
-        return true;
+        outcome = true;
       }
     }
   } else {
-    return false;
+    return outcome;
   }
+  return outcome;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
+module.exports = eqArrays;
