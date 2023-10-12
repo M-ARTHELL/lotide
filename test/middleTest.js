834f604490190766a6e1a143c726cf91238b804e
M-ARTHELL <makoarthell@proton.me>
2023-10-12 20:54:12 -0300

    added tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,24 @@
 const assert = require('chai').assert;
-const middle = require('../middle');
+const _ = require('../index');
 
 describe("#middle", () => {
   it("returns [] for [1]", () => {
-    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(_.middle([1]), []);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(_.middle([1, 2]), []);
   });
 
   it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3]), [2]); 
+    assert.deepEqual(_.middle([1, 2, 3]), [2]); 
   });
 
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+    assert.deepEqual(_.middle([1, 2, 3, 4]), [2, 3]); 
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(_.middle([]), []); 
   });
 });
