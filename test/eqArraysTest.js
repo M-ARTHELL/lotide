5f37674fe3dda38c2caed470f3b462f9c9cda1af
M-ARTHELL <makoarthell@proton.me>
2023-10-12 17:17:34 -0300

    set up tests for mocha/chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,18 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const _ = require('../index');
 
-//TEST CASES
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); //true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); //false
+
+describe("#countLetters", () => {
+  it("returns 'true' if input arrays are equal", () => {
+    assert.deepEqual(_.eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns 'false' if input arrays are not equal", () => {
+    assert.deepEqual(_.eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("returns 'false' if input arrays differ by data type", () => {
+    assert.deepEqual(_.eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it("returns 'true' if both arrays are empty", () => {
+    assert.deepEqual(_.eqArrays([], []), true);
+  });
+});
