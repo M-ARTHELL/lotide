37cd499726718484973feb72043aea8a14a1ed5e
M-ARTHELL <makoarthell@proton.me>
2023-10-12 17:15:19 -0300

    restructured, fixed bug with two blank arrays returning false

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,28 @@
-//assertEquals
-const assertEqual = require('./assertEqual');
+const _ = require('./index');
 
-//eqArrays
+// eqArrays
 const eqArrays = function(arr1, arr2) {
   let outcome = false;
-  //Checks if array lengths are equal, returns false if not.
+
+  // checks if array lengths are equal
   if (arr1.length === arr2.length) {
+
+    // returns true if both arrays are empty, as it would return false otherwise
+    if (arr1.length === 0 && arr2.length === 0) {
+      outcome = true;
+      return outcome;
+    }
+
     for (let i = 0; i < arr1.length; i++) {
-      //If array elements don't match, returns false. Otherwise, returns true.
+      // if array elements don't match, returns false. Otherwise, returns true.
       if (arr1[i] !== arr2[i]) {
         return false;
       } else {
         outcome = true;
       }
     }
-  } else {
-    return outcome;
   }
+  // returns false if lengths are not equal
   return outcome;
 };
 
