8ca077487cd88d99d1cfcb363e0695c441ba804e
M-ARTHELL <makoarthell@proton.me>
2023-10-12 20:03:26 -0300

    added more tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,21 @@
 const assert = require('chai').assert;
-const head   = require('../head');
+const _ = require('../index');
+
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
+    assert.strictEqual(_.head([1, 2, 3]), 1);
   });
 
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+  it("returns 'string' for ['string', 'another string']", () => {
+    assert.strictEqual(_.head(['string', 'another string']), 'string'); 
+  });
+
+  it("returns 'single' for ['single']", () => {
+    assert.strictEqual(_.head(['single']), 'single'); 
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(_.head([]), undefined); 
   });
 });
