23356045ad03752e6444979700a52cfd3ba0441b
M-ARTHELL <makoarthell@proton.me>
2023-10-12 20:52:43 -0300

    simplified response for too short arrays

--- middle.js
+++ middle.js
@@ -1,24 +1,23 @@
-//assertArraysEqual copy
-const assertArraysEqual = require('./assertArraysEqual');
+const _ = require('./index');
 
-//eqArrays copy
-const eqArrays = require('./assertArraysEqual');
-
-//middle
+// middle
+// takes in an array and returns the middlemost element
 const middle = function(array) {
   let arrayMiddle = [];
-  //Returns empty array if it's too short.
-  if (array.length === 1 || array.length === 2) {
+
+  // returns empty array if the input array is too short
+  if (array.length <= 2) {
     return arrayMiddle;
-    //Push two middle elements for even arrays.
+
+    // pushes the two middle elements for even arrays
   } else if (array.length % 2 === 0) {
     arrayMiddle.push(array[(array.length / 2) - 1]);
     arrayMiddle.push(array[array.length / 2]);
+
   } else {
-    //Push middle elements for other arrays
+    // pushes the one middle element for odd arrays
     arrayMiddle.push(array[Math.floor(array.length / 2)]);
   }
-  //Print middle arrays to console.
   return arrayMiddle;
 };
 
