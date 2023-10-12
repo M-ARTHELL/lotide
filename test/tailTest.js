8936b363b5af2a00da601a402b1187227f62170c
M-ARTHELL <makoarthell@proton.me>
2023-10-12 20:17:29 -0300

    added tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,20 @@
 const assert = require('chai').assert;
-const tail = require('../tail');
-
+const _ = require('../index');
+ 
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
-    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+    assert.deepEqual(_.tail([1, 2, 3]), [2, 3]);
   });
 
   it("returns [5, 6] for [14, 5, 6]", () => {
-    assert.deepEqual(tail([4, 5, 6]), [5, 6]); 
+    assert.deepEqual(_.tail([4, 5, 6]), [5, 6]); 
+  });
+
+  it("returns ['bonk'] for ['bink', 'bonk']", () => {
+    assert.deepEqual(_.tail(['bink', 'bonk']), ['bonk']); 
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(_.tail([]), []);
   });
 });
