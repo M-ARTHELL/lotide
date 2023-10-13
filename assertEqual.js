7d826c0b85f160f846baa93ae02c22a117d44cff
M-ARTHELL <makoarthell@proton.me>
2023-10-13 01:56:04 -0300

    added comment, made more legible

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,13 @@
-// FUNCTION IMPLEMENTATION
+const _ = require('./index');
+
+
+// assertEqual
+// put in two primitives and compares them, then console.logs the result
 const assertEqual = function(actual, expected) {
+
   if (actual === expected) {
     return console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
+
   } else {
     return console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
   }
