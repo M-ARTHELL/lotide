4996a09c8918ffe4e54201eb16e083769e8084e6
M-ARTHELL <makoarthell@proton.me>
2023-10-13 02:00:49 -0300

    added comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,24 @@
-//eqArrays
-const eqArrays = require('./eqArrays');
+const _ = require('./index');
 
+
+// assertArraysEqual
+// takes in two arrays and console.logs the result
 const assertArraysEqual = function(actual, expected) {
   const fail = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   const pass = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
 
-  if (eqArrays(actual,expected) === true) {
+  // passes contents to eqArrays, returns a pass or fail
+  if (_.eqArrays(actual,expected) === true) {
     return console.log(pass);
   } else {
     return console.log(fail);
   }
 };
+
+/*
+// tests
+assertArraysEqual([1, 2], [1, 2]);
+assertArraysEqual([1, 2], [2, 1]);
+*/
 
 module.exports = assertArraysEqual;
