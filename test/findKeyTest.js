8995ea498c4b14c264648829b4dd0d5450c5bcc2
M-ARTHELL <makoarthell@proton.me>
2023-10-13 01:40:55 -0300

    set up mocha/chai tests

---
+++ test/findKeyTest.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const _ = require('../index');
+
+  describe("#findKey", () => {
+    it("returns `noma` when entering {`Blue Hill`: { stars: 1 },`Akaleri`: { stars: 3 },`noma`: { stars: 2 }} and (x => x.stars === 2)", () => {
+      assert.equal(_.findKey({ "Blue Hill": { stars: 1 }, "Akaleri": { stars: 3 },"noma": { stars: 2 } }, x => x.stars === 2), "noma");
+    });
+
+    it("returns false when entering { `Blue Hill`: { stars: 1 }, `Akaleri`: { stars: 3 },`noma`: { stars: 23 } } and (x => x.stars === 2)", () => { 
+      assert.equal(_.findKey({ "Blue Hill": { stars: 1 }, "Akaleri": { stars: 3 },"noma": { stars: 23 } }, x => x.stars === 2), undefined);
+    });
+
+    it("returns `Shirt` when entering { Shirt: { color: `red`, size: 5}, Pants: { color: `blue`, size: 7} } and (x => x.color === `red`)", () => { 
+        assert.equal(_.findKey({ Shirt: { color: "red", size: 5}, Pants: { color: "blue", size: 7} }, x => x.color === "red"), "Shirt");
+    });
+
+    it("returns undefined when entering { Shirt: { color: `red`, size: 5}, Pants: { color: `blue`, size: 7} } and (x => x.color === `green`)", () => { 
+        assert.equal(_.findKey({ Shirt: { color: "red", size: 5}, Pants: { color: "blue", size: 7} }, x => x.color === "green"), undefined);
+    });
+  });
