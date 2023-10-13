0f70c4683a8d2ba7455a6be14b1cfbae15bce5ed
M-ARTHELL <makoarthell@proton.me>
2023-10-13 01:39:25 -0300

    separated tests for mocha/chai, added comments

--- findKey.js
+++ findKey.js
@@ -1,40 +1,22 @@
-// assertEquals
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
-  } else {
-    return console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
-  }
-};
+const _ = require('./index');
 
 
-//findKey
+// findKey
+// takes an object and a callback, then returns a key that contains the specifications of the callback
 const findKey = function(obj, callback) {
+
+  // keys are listed in an array for reference
   const keys = Object.keys(obj);
   let notFound = undefined
+
+  // compares the callback to each key, returns it if a match is found
   for (let key of keys) {
     if (callback(obj[key])) {
       return key;
     }
   }
+  // returns 'undefined' if no match
   return notFound;
 };
 
-//test
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"); // => "noma")
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 23 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 12 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), undefined); // => "undefined"
+module.exports = findKey;
