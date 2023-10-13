5c49956516c682a6933322020489ff8b6f123e3e
M-ARTHELL <makoarthell@proton.me>
2023-10-13 00:44:29 -0300

    separated tests for mocha/chai, added comments, made case for empty array

--- takeUntil.js
+++ takeUntil.js
@@ -1,62 +1,20 @@
-//eqArrays
-const eqArrays = function(arr1, arr2) {
-  let outcome = false;
-  if (arr1.length === arr2.length) {
-    if (arr1.length === 0) {
-      outcome = true;
-    }
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } else {
-        outcome = true;
-      }
-    }
-  } else {
-    return outcome;
-  }
-  return outcome;
-};
+const _ = require('./index');
 
 
-//assertArraysEqual
-const assertArraysEqual = function(actual, expected) {
-  const fail = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  const pass = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-
-  if(eqArrays(actual, expected) === true) {
-    return console.log(pass)
-  } else {
-    return console.log(fail)
-  }
-};
-
-
-//takeUntil
+// takeUntil
+// takes in an array and a callback, then returns the array, cut off at the point specified by the callback
 const takeUntil = function(array, callback) {
+
+  // if the array is empty, returns the empty array
+  if(array.length === 0) {
+    return array;
+  }
   for (let i = 0; i < array.length; i++) {
+    // if the current element satisfies the callback's specifications, the array is cut off there and returned
     if (callback(array[i])) {
       return array.slice(0, i);
     }
   }
 };
 
-
-//TESTING
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ])
-
-console.log('---');
-
-const data3 = [0, 3, 2, 7, 5]
-const results3 = takeUntil(data3, x => x === 0);
-assertArraysEqual(results3, []);
+module.exports = takeUntil
