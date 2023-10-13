c936821b28ea48ca8a1af814b7b335366c495c5c
M-ARTHELL <makoarthell@proton.me>
2023-10-13 00:56:47 -0300

    separated testing for mocha/chai, added comments

--- without.js
+++ without.js
@@ -1,47 +1,22 @@
-//eqArrays copy
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  } else {
-    return false;
-  }
-};
+const _ = require('./index');
 
-//assertEqual copy
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    return console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
-  } else {
-    return console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
-  }
-};
 
+// without
+// takes in an array and an array with unwanted elements, then returns a copy of thefirst array with the elements removed
 const without = function(array, toRemove) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
-    //If element in array at index is in toRemove do nothing.
+
+    //if current element is in 'toRemove', do nothing
     if (toRemove.includes(array[i])) {
       // (nothing)
     } else {
-    //Push other elements to newArray.
+      // if element is not in 'toRemove', push it to the new array
       newArray.push(array[i]);
     }
   }
-  //Return newArray.
+  // return newArray.
   return newArray;
 };
 
-//Tests
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-without([1, 2, 3], [1]); // => [2, 3]
-assertArraysEqual([1, 2, 3], [1]);
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
-assertArraysEqual(["1", "2", "3"], [1, 2, "3"]);
+module.exports = without;
